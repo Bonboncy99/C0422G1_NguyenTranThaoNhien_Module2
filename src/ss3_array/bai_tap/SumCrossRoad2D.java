d6c074fd1a8dc954a8ff684c4b4dc79f2389b1e0
Bonboncy99 <thaonhien9991@gmail.com>
2022-06-13 22:50:12 +0700

    Ss3 Array bổ sung bài tập *

--- src/ss3_array/bai_tap/SumCrossRoad2D.java
+++ src/ss3_array/bai_tap/SumCrossRoad2D.java
@@ -6,35 +6,47 @@ import java.util.Scanner;
 public class SumCrossRoad2D {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+//        Tạo mảng 2 chiều
         System.out.println("Nhập độ dài mảng");
-        int size1 = Integer.parseInt(scanner.nextLine());
-        System.out.println("Nhập độ dài mảng con");
-        int size2 = Integer.parseInt(scanner.nextLine());
-        int [][] arr2d = new int[size1][size2];
-        for (int i = 0; i < size1; i++) {
-            for (int j = 0; j < size2; j++) {
-                System.out.println("Nhập phần tử " + (j+1));
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] arr2d = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.println("Nhập phần tử " + (j + 1));
                 arr2d[i][j] = Integer.parseInt(scanner.nextLine());
             }
         }
-        for (int i = 0; i < size1; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.println(Arrays.toString(arr2d[i]));
         }
 
+//        tính tổng 2 đường chéo
         int sum = 0;
-        for (int i = 0; i < size1 -1; i++) {
-                sum += arr2d[i][i];
-            }
-        for (int i = size1-1; i >= 0 ; i--) {
+
+        // Đường chéo \
+        for (int i = 0; i < size; i++) {
             sum += arr2d[i][i];
+            System.out.print(arr2d[i][i] + ", ");
         }
-        if (size1 % 2 != 0) {
-            sum -= arr2d[(size1-1)/2][(size1-1)/2];
+        System.out.println();
+
+        // đường chéo /
+        for (int i = 0; i < size; i++) {
+            sum += arr2d[i][size - 1 - i];
+            System.out.print(arr2d[i][size - 1 - i] + ", ");
         }
-        System.out.println(arr2d[(size1-1)/2][(size1-1)/2]);
+        System.out.println();
+
+
+        //nếu mảng có size là số lẻ thì trừ đi phần tử ở giữa
+        if (size % 2 != 0) {
+            sum -= arr2d[(size - 1) / 2][(size - 1) / 2];
+            System.out.println(arr2d[(size - 1) / 2][(size - 1) / 2]);
+        }
+
         System.out.println(sum);
-        }
-
-
     }
+
+}
 
