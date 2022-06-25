09f4b8e564b72d8f73377ad4868bf5f2dcef936b
Bonboncy99 <thaonhien9991@gmail.com>
2022-06-25 10:31:57 +0700

    SS15  Sort Algorithm

---
+++ src/ss14_debug_xu_ly_ngoai_le/bai_tap/lamlai/Run.java
@@ -0,0 +1,2 @@
+package ss14_debug_xu_ly_ngoai_le.bai_tap.lamlai;public class Run {
+}
---
+++ src/ss15_sort_algorithm/thuc_hanh/bai_tap/InsertSortByStep.java
@@ -0,0 +1,25 @@
+package ss15_sort_algorithm.thuc_hanh.bai_tap;
+
+import java.util.Arrays;
+
+public class InsertSortByStep {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    private static void insertionSort(int[] arr) {
+        int pos;
+        int x;
+        for (int i = 1; i < arr.length; i++) {
+            pos = i;
+            x = arr[i];
+            while (pos > 0 && x < arr[pos-1]){
+                arr[pos] = arr[pos -1];
+                pos--;
+            }
+            arr[pos] = x;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+    public static void main(String[] args) {
+        insertionSort(list);
+        System.out.println(Arrays.toString(list));
+    }
+}
