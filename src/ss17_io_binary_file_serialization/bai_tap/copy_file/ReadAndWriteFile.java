33401a4e45cf9e6d2bfcedea480f00ac0f308a6e
Bonboncy99 <thaonhien9991@gmail.com>
2022-06-28 17:20:57 +0700

    ss17

---
+++ src/ss17_io_binary_file_serialization/bai_tap/copy_file/ReadAndWriteFile.java
@@ -0,0 +1,70 @@
+package ss17_io_binary_file_serialization.bai_tap.copy_file;
+
+import ss17_io_binary_file_serialization.bai_tap.product.WriteAndReadFile;
+import ss17_io_binary_file_serialization.bai_tap.product.models.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public static void writeToFile(String path, List<Product> products) {
+        File file = new File(path);
+        FileOutputStream fileOutputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(products);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileOutputStream.close();
+                objectOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static List<Product> readFromFile(String path) {
+        List<Product> products = new ArrayList<>();
+        File file = new File(path);
+        FileInputStream fileInputStream = null;
+        ObjectInputStream objectInputStream = null;
+        try {
+            if (file.length() > 0) {
+                fileInputStream = new FileInputStream(file);
+                objectInputStream = new ObjectInputStream(fileInputStream);
+                products = (List<Product>) objectInputStream.readObject();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (file.length() > 0) {
+                    fileInputStream.close();
+                    objectInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return products;
+    }
+
+    public static void main(String[] args) {
+        final String FILE_PATH2 = "src/ss17_io_binary_file_serialization/bai_tap/copy_file/file_copy.dat";
+        writeToFile(FILE_PATH2, readFromFile("ss17_io_binary_file_serialization/bai_tap/product/product.dat"));
+    }
+}
